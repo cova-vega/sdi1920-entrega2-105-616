0abd0976ca88bd4d74f75e7b52bc5dd17ce6b61e
cova-vega <uo257507@uniovi.es>
2020-04-16 18:29:54 +0200

    Identificación de usuario y desconexión

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -3,10 +3,22 @@ module.exports = function (app, swig, gestorBD) {
     app.get("/usuarios", function (req, res) {
         res.send("ver usuarios");
     });
+
     app.get("/registrarse", function (req, res) {
         var respuesta = swig.renderFile('views/bregistro.html', {});
         res.send(respuesta);
 
+    });
+
+    app.get("/identificarse", function (req, res) {
+        var respuesta = swig.renderFile('views/bidentificacion.html', {});
+        res.send(respuesta);
+
+    });
+
+    app.get('/desconectarse', function (req, res) {
+        req.session.usuario = null;
+        res.send("Usuario desconectado");
     });
 
     app.post('/usuario', function (req, res) {
@@ -14,13 +26,35 @@ module.exports = function (app, swig, gestorBD) {
             .update(req.body.password).digest('hex');
         let usuario = {
             email: req.body.email,
+            nombre : req.body.nombre,
+            apellidos : req.body.apellidos,
             password: seguro
         };
+
         gestorBD.insertarUsuario(usuario, function (id) {
             if (id == null) {
                 res.send("Error al insertar ");
             } else {
                 res.send('Usuario Insertado ' + id);
+            }
+        });
+
+    });
+    app.post('/identificarse', function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio = {
+            email: req.body.email,
+            password: seguro
+        };
+
+        gestorBD.obtenerUsuario(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                req.session.usuario = null;
+                res.send("Error al loguarse ");
+            } else {
+                req.session.usuario =  usuarios[0].email;
+                res.send('Usuario identificado ');
             }
         });
 
