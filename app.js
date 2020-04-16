e26e2a727a264acbe39179e85f24e02de73cfe53
cova-vega <uo257507@uniovi.es>
2020-04-16 15:33:05 +0200

    Primeras operaciones con la base de datos MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 //Modulos
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let mongo = require('mongodb');
 
-var swig = require('swig');
-var bodyParser = require('body-parser');
+let swig = require('swig');
+let bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//Variables
+app.set('port', 8081);
+app.set('db',' mongodb://admin:sdi@socialnetwork-shard-00-00-fld0u.mongodb.net:27017,socialnetwork-shard-00-01-fld0u.mongodb.net:27017,socialnetwork-shard-00-02-fld0u.mongodb.net:27017/test?ssl=true&replicaSet=SocialNetwork-shard-0&authSource=admin&retryWrites=true&w=majority');
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig, mongo);
+
 
 //Server
-app.set('port', 8081);
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
 });
